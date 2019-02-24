4589544bcf2160bfbeadc7b3046f6402989cb906
KamishiroArisu <binyang.dby@gmail.com>
2019-02-24 15:55:56 +0800

    [EX3.1]Add Exercise 3.1

---
+++ src/test/scala/functional/programming/in/scala/chapter3/Exercise1Test.scala
@@ -0,0 +1,9 @@
+package functional.programming.in.scala.chapter3
+
+import org.scalatest.FunSuite
+
+class Exercise1Test extends FunSuite {
+    test("result") {
+        assert(Exercise1.result == 3)
+    }
+}
