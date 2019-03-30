4319985e72c3e1c8160e42f3ac9b59413df54228
KamishiroArisu <binyang.dby@gmail.com>
2019-03-30 11:39:57 +0800

    [EX7.1]Add Exercise 7.1

---
+++ src/main/scala/functional/programming/in/scala/chapter7/Exercise1.scala
@@ -0,0 +1,5 @@
+package functional.programming.in.scala.chapter7
+
+object Exercise1 {
+    //def map2[A,B,C](p1: Par[A], p2: Par[B])(f: (A, B) => C): Par[C]
+}
---
+++ src/test/scala/functional/programming/in/scala/chapter7/Exercise1Test.scala
@@ -0,0 +1,7 @@
+package functional.programming.in.scala.chapter7
+
+import org.scalatest.FunSuite
+
+class Exercise1Test extends FunSuite {
+
+}
