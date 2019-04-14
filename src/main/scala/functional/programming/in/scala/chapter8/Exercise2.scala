e94a44fa0a07cad48dea20812d67111d8d2e1ed9
KamishiroArisu <binyang.dby@gmail.com>
2019-04-14 17:20:22 +0800

    [EX8.2]Add Exercise 8.2

---
+++ src/main/scala/functional/programming/in/scala/chapter8/Exercise2.scala
@@ -0,0 +1,8 @@
+package functional.programming.in.scala.chapter8
+
+object Exercise2 {
+    //1. The maximum should be an element in list
+    //2. The maximum is undefined for a empty list
+    //3. The maximum should be greater than or equals to every element in list
+    //...
+}
---
+++ src/test/scala/functional/programming/in/scala/chapter8/Exercise2Test.scala
@@ -0,0 +1,7 @@
+package functional.programming.in.scala.chapter8
+
+import org.scalatest.FunSuite
+
+class Exercise2Test extends FunSuite {
+
+}
