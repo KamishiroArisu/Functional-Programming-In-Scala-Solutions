3caf4e9df28bb9ff11d86d8a98616e19a64c5ff3
KamishiroArisu <binyang.dby@gmail.com>
2019-04-14 17:09:38 +0800

    [EX8.1]Add Exercise 8.1

---
+++ src/main/scala/functional/programming/in/scala/chapter8/Exercise1.scala
@@ -0,0 +1,8 @@
+package functional.programming.in.scala.chapter8
+
+object Exercise1 {
+    //1. Reversing a list and summing it should give the same result as summing the original, nonreversed list.
+    //2. The sum should be head multiply length if list is nonempty and all elements of the list are the same value
+    //3. The sum should be 0 if list is empty
+    //...
+}
